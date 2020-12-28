a3380337ba4ede5836258f63f729e8d56c3aad13
DEKG <2538315452@qq.com>
2020-12-28 17:07:47 +0900

    冲突测试03

--- src/main/java/com/wanghao/githubtest/entity/myTestEntity_02.java
+++ src/main/java/com/wanghao/githubtest/entity/myTestEntity_02.java
@@ -3,6 +3,6 @@ package com.wanghao.githubtest.entity;
 public class myTestEntity_02 {
 
     public static void main(String[] args) {
-        System.out.println("main");
+        System.out.println("niam");
     }
 }
